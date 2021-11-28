a4e984f19bed7b9db745879c27b147c93b54939d
sky0078 <94841675+sky0078@users.noreply.github.com>
2021-11-28 13:23:08 +0100

    Update admin.py

--- first/bages/admin.py
+++ first/bages/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import User , Message, Write_msg
+from .models import  Messages, Write_msg
 
 # Register your models here.
 
-admin.site.register(User)
-admin.site.register(Message)
+admin.site.register(Messages)
 admin.site.register(Write_msg)
